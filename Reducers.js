9c90790bc5e77ec230f1f1c03e1c2228394de244
bahadirkorumaz <bahadir.korumaz@gmail.com>
2020-03-02 22:52:19 +0300

    created first reducer

---
+++ Reducers.js
@@ -0,0 +1,29 @@
+const createPolicy = (name, amount) => {
+    return {
+        type : 'CREATE_POLICY',
+        payload: {
+            name : name,
+            money : amount
+        }
+    };
+};
+
+const deletePolicy = (name) => {
+    return { 
+        type : 'DELETE_POLICY',
+        payload : {
+            name: name
+        }
+    };
+};
+
+
+const createClaim = (name, amountOfMoneyToCollect) => {
+    return {
+        type: 'CREATE_CLAIM',
+        payload : {
+            name : name,
+            amountOfMoneyToCollect : amountOfMoneyToCollect
+        }
+    };
+};
